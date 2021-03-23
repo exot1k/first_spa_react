42af0fb8e664b4e001d970b9e87b8513f93f5024
Exotik <sashakaterinochkin@mail.ru>
2021-03-23 22:43:31 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+    <header className={'header'}>
+        <img
+            src='https://e7.pngegg.com/pngimages/922/865/png-clipart-discord-pepe-the-frog-video-games-pepe.png'/>
+    </header>
+    );
+}
+
+export default  Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className={'content'}>
+            <div>
+                <img
+                    src='https://spng.pngfind.com/pngs/s/475-4759930_pepe-meme-rarepepe-slav-russian-pepe-the-frog.png'/>
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                My Post
+                <div>
+                    new posts
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default  Profile;
