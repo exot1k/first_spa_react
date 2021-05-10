1cdd283966f933736b62afb8ae8fd43ebdde7661
Exotik <sashakaterinochkin@mail.ru>
2021-05-10 15:05:39 +0300

    Видео 95 Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import Dialogs from "./components/Dialogs/Dialogs";
 import {Route} from "react-router-dom";
-import Music from "./components/Music/Music";
-import News from "./components/News/News";
-import Settings from "./components/Settings/Settings";
 import SecondNavbar from "./components/SecondNavBar/SecondNavbar";
-import Friends from "./components/Friends/Friends";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect} from "react-redux";
 import {initializeApp} from "./Redux/AppReducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
+
+const Music = React.lazy(() => import('./components/Music/Music'));
+const News = React.lazy(() => import('./components/News/News'));
+const Settings = React.lazy(() => import('./components/Settings/Settings'));
+const Friends = React.lazy(() => import('./components/Friends/Friends'));
+const Dialogs = React.lazy(() => import('./components/Dialogs/Dialogs'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
     componentDidMount() {
@@ -32,13 +34,13 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className={"app-wrapper-content"}>
-                    <Route exact path={"/dialogs"} render={() => <Dialogs/>}/>
-                    <Route path={"/profile/:userId?"} render={() => <ProfileContainer/>}/>
-                    <Route path={"/news"} render={() => <News/>}/>
-                    <Route path={"/music"} render={() => <Music/>}/>
-                    <Route path={"/settings"} render={() => <Settings/>}/>
-                    <Route path={"/users"} render={() => <UsersContainer/>}/>
-                    <Route path={"/friends"} render={() => <Friends/>}/>
+                    <Route exact path={"/dialogs"} render={withSuspense(Dialogs)}/>
+                    <Route path={"/profile/:userId?"} render={withSuspense(ProfileContainer)}/>
+                    <Route path={"/news"}  render={withSuspense(News)} />
+                    <Route path={"/music"} render={withSuspense(Music)} />
+                    <Route path={"/settings"} render={withSuspense(Settings)} />
+                    <Route path={"/users"} render={withSuspense(UsersContainer)} />
+                    <Route path={"/friends"} render={withSuspense(Friends)}/>
                     <Route path={"/login"} render={() => <Login/>}/>
                 </div>
                 <SecondNavbar/>
